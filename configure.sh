09506a55891bcb37e77f054b5094e757cbf7fe38
Zivision <zcastro703@gmail.com>
2026-01-01 09:17:47 -0600

    Added EXWM

--- configure.sh
+++ configure.sh
@@ -3,3 +3,6 @@
 # Runs stow and loads folders into the .config dir
 stow doom -t ~/.config/doom
 stow home -t ~
+
+# Copy desktop file over to xsessions
+sudo cp ~/Workspace/Misc/configs/doom/exwm/exwm.desktop /usr/share/xsessions/
