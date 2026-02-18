5a631c7e69d2cdc832ed862c248708c1975af871
Zivision <zcastro703@gmail.com>
2026-02-18 08:37:13 -0600

    fix: Added ocaml

--- doom/init.el
+++ doom/init.el
@@ -125,7 +125,7 @@
        ;;gdscript          ; the language you waited for
        (go +lsp)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
-       (haskell +lsp)    ; a language that's lazier than I am
+       ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
@@ -142,7 +142,7 @@
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
-       ;;ocaml             ; an objective camel
+       ocaml             ; an objective camel
        (org
         +roam2)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
