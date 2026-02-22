30e827dd636d5723319c1be800aae06ed076d29c
Zivision <zcastro703@gmail.com>
2026-02-22 09:08:40 -0600

    feat: added emacs daemon to nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -99,6 +99,11 @@ networking.firewall.enable = true;
 # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
 system.stateVersion = "25.05"; # Did you read the comment?
 
+services.emacs = {
+  enable = true;
+  package = pkgs.emacs;
+};
+
   # Enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
