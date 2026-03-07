f1dcc7f5d57fd035e8921134dc794c547fc47cf6
Zivision <zcastro703@gmail.com>
2026-03-07 09:31:57 -0600

    feat: added syncthing

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -102,6 +102,12 @@ services.emacs = {
    defaultEditor = true;
 };
 
+# Example for /etc/nixos/configuration.nix
+services.syncthing = {
+  enable = true;
+  openDefaultPorts = true; # Open ports in the firewall for Syncthing. (NOTE: this will not open syncthing gui port)
+};
+
 programs.hyprland.enable = true;
 
 # Optional, hint Electron apps to use Wayland:
