f73226217353f2170d5a45e01127a8f962fd7f67
Zivision <zcastro703@gmail.com>
2026-05-06 13:48:04 -0500

    fix: language clean

--- build/doom/init.el
+++ build/doom/init.el
@@ -104,8 +104,8 @@ terraform         ; infrastructure as code
 ;;agda              ; types of types of types of types...
 ;;beancount         ; mind the GAAP
 ;;(cc +lsp)         ; C > C++ == 1
-clojure           ; java with a lisp
-common-lisp       ; if you've seen one lisp, you've seen them all
+;;clojure           ; java with a lisp
+;;common-lisp       ; if you've seen one lisp, you've seen them all
 ;;coq               ; proofs-as-programs
 ;;crystal           ; ruby at the speed of c
 ;;csharp            ; unity, .NET, and mono shenanigans
@@ -125,7 +125,7 @@ emacs-lisp        ; drown in parentheses
 ;;gdscript          ; the language you waited for
 (go +lsp)         ; the hipster dialect
 ;;(graphql +lsp)    ; Give queries a REST
-(haskell +lsp)    ; a language that's lazier than I am
+;;(haskell +lsp)    ; a language that's lazier than I am
 ;;hy                ; readability of scheme w/ speed of python
 ;;idris             ; a language you can depend on
 json              ; At least it ain't XML
@@ -143,7 +143,7 @@ latex             ; writing papers in Emacs has never been so fun
 markdown          ; writing docs for people to ignore
 ;;nim               ; python + lisp at the speed of c
 nix               ; I hereby declare "nix geht mehr!"
-;;ocaml             ; an objective camel
+ocaml             ; an objective camel
 (org
  +roam)               ; organize your plain life in plain text
 ;;php               ; perl's insecure younger brother
@@ -158,7 +158,7 @@ nix               ; I hereby declare "nix geht mehr!"
 ;;rest              ; Emacs as a REST client
 ;;rst               ; ReST in peace
 ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
 ;;scala             ; java, but good
 ;;(scheme +guile)   ; a fully conniving family of lisps
 sh                ; she sells {ba,z,fi}sh shells on the C xor
