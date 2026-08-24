d0e7b76e9a9ca1bcdc7fd3dbf8988ab002774649
Zivision <zcastro703@gmail.com>
2026-08-24 14:36:57 -0500

    flake fix

--- flake.nix
+++ flake.nix
@@ -1,127 +1,125 @@
 {
   description = "My system flake";
 
-  inputs = {
-    # NixOS official package source, using the nixos-26.05 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+inputs = {
+  # NixOS official package source, using the nixos-26.05 branch here
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
-    # Nixos WSL Channel
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-    # Home manager
-    home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  # Nixos WSL Channel
+  nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+  # Home manager
+  home-manager = {
+    url = "github:nix-community/home-manager/release-26.05";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
+};
 
-  outputs =
+outputs = { self, nixpkgs, home-manager, nixos-wsl, ... }@inputs:
+let
+  system = "x86_64-linux";
+  legacyPkgs = nixpkgs.legacyPackages.${system};
+in
+{
+
+nixosConfigurations = {
+
+swiftx = nixpkgs.lib.nixosSystem {
+  modules = [
+    # Import the previous configuration.nix we used,
+    # so the old configuration file still takes effect
+    ./build/nixos/configuration.nix
+    ./build/nixos/nvidia.nix
+    ./build/nixos/swiftx.nix
+
+    # Import home manager
+    home-manager.nixosModules.home-manager
     {
-      self,
-      nixpkgs,
-      home-manager,
-      ...
-    }@inputs:
-    let
-      system = "x86_64-linux";
-      legacyPkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.primary =  {
+        imports = [
 
-      nixosConfigurations = {
-
-        swiftx = nixpkgs.lib.nixosSystem {
-          modules = [
-            # Import the previous configuration.nix we used,
-            # so the old configuration file still takes effect
-            ./build/nixos/configuration.nix
-            ./build/nixos/nvidia.nix
-            ./build/nixos/swiftx.nix
-
-            # Import home manager
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.primary = {
-                imports = [
-
-                  ./build/nixos/home.nix
-                  ./build/nixos/dev.nix
-                ];
-              };
-            }
-          ];
-        };
-
-        nixbox = nixpkgs.lib.nixosSystem {
-          modules = [
-            # Import the previous configuration.nix we used,
-            # so the old configuration file still takes effect
-            ./build/nixos/configuration.nix
-            ./build/nixos/nixbox.nix
-
-            # Import home manager
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.primary = {
-                imports = [
-
-                  ./build/nixos/home.nix
-                  ./build/nixos/dev.nix
-                ];
-              };
-            }
-          ];
-        };
-      };
-
-      wsl = nixpkgs.lib.nixosSystem {
-        modules = [
-          # Import the previous configuration.nix we used,
-          # so the old configuration file still takes effect
-          ./build/nixos/wsl.nix
-
-          # Import home manager
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.primary = {
-              imports = [
-                ./build/nixos/dev.nix
-              ];
-            };
-          }
+        ./build/nixos/home.nix
+        ./build/nixos/dev.nix
         ];
       };
+    }
+  ];
+};
 
-      devShells.${system} = {
+nixbox = nixpkgs.lib.nixosSystem {
+  modules = [
+    # Import the previous configuration.nix we used,
+    # so the old configuration file still takes effect
+    ./build/nixos/configuration.nix
+    ./build/nixos/nixbox.nix
 
-        default = legacyPkgs.mkShell {
-          packages = with legacyPkgs; [
-            pyright
-            black
-            python313Packages.pyflakes
-            python313Packages.isort
-          ];
-          shellHook = ''
-            echo "Dev Envrionment Ready!"
-          '';
-        };
+    # Import home manager
+    home-manager.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.primary =  {
+        imports = [
 
-        install = legacyPkgs.mkShell {
-          packages = with legacyPkgs; [
-            git
-            emacs
-            python3
-          ];
-          shellHook = ''
-            echo "Ready to build new nix machine!"
-          '';
-        };
-
+        ./build/nixos/home.nix
+        ./build/nixos/dev.nix
+        ];
       };
-    };
+    }
+  ];
+};
+
+wsl = nixpkgs.lib.nixosSystem {
+  modules = [
+    # Import the previous configuration.nix we used,
+    # so the old configuration file still takes effect
+
+    nixos-wsl.nixosModules.default
+    {
+      system.stateVersion = "26.05";
+      wsl.enable = true;
+    }
+    # Import home manager
+    home-manager.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.primary =  {
+        imports = [
+          ./build/nixos/dev.nix
+        ];
+      };
+    }
+  ];
+};
+};
+
+devShells.${system}= {
+
+default = legacyPkgs.mkShell {
+  packages = with legacyPkgs; [
+    pyright
+    black
+    python313Packages.pyflakes
+    python313Packages.isort
+  ];
+  shellHook = ''
+    echo "Dev Envrionment Ready!"
+  '';
+};
+
+install = legacyPkgs.mkShell {
+  packages = with legacyPkgs; [
+    git
+    emacs
+    python3
+  ];
+  shellHook = ''
+    echo "Ready to build new nix machine!"
+  '';
+};
+
+};
+};
 }
